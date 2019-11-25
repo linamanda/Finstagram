7cad23b497504b2aa29edeccf646c50d0f1069fa
Amanda <amanda.lin.aut@gmail.com>
2019-11-24 21:02:18 -0500

    Home page lightly redesigned. New user registration corrected and works as it should. Images function to retrieve images that a specific user is allowed to see completed and works as it should. Images page edited to display images correctly, and to display photoID, photoPoster, and postingdate. Upload Image function completed so user can enter the location of a photo on their computer, a designation of whether the photo is visible to all followers or only to members of designated friend groups, and a caption. Finstagram inserts data about the photo including postingdate, photoID, and photoPoster, and users can choose which friend groups they belong in that they want to share the photo with. Upload page adjusted to be able to perform the Upload Image function.

--- app.py
+++ app.py
@@ -12,13 +12,12 @@ IMAGES_DIR = os.path.join(os.getcwd(), "images")
 
 connection = pymysql.connect(host="localhost",
                              user="root",
-                             password="",
-                             db="finsta",
+                             password="vanillaroses",
+                             db="finstagram",
                              charset="utf8mb4",
                              port=3308,
                              cursorclass=pymysql.cursors.DictCursor,
                              autocommit=True)
-
 
 def login_required(f):
     @wraps(f)
@@ -26,9 +25,7 @@ def login_required(f):
         if not "username" in session:
             return redirect(url_for("login"))
         return f(*args, **kwargs)
-
     return dec
-
 
 @app.route("/")
 def index():
@@ -36,28 +33,33 @@ def index():
         return redirect(url_for("home"))
     return render_template("index.html")
 
-
 @app.route("/home")
 @login_required
 def home():
     return render_template("home.html", username=session["username"])
 
-
 @app.route("/upload", methods=["GET"])
 @login_required
 def upload():
-    return render_template("upload.html")
+    query = "SELECT * FROM belongto WHERE member_username = %s"
+    with connection.cursor() as cursor:
+        cursor.execute(query, (session["username"]))
+    data = cursor.fetchall()
 
+    return render_template("upload.html", groups=data)
 
 @app.route("/images", methods=["GET"])
 @login_required
 def images():
-    query = "SELECT * FROM photo"
+    query = "SELECT * FROM photo AS p1 WHERE (allFollowers = TRUE AND %s = (SELECT username_follower FROM follow " \
+            "WHERE username_followed = p1.photoPoster AND username_follower = %s AND followStatus = TRUE)) OR " \
+            "(%s IN (SELECT member_username FROM belongto WHERE owner_username = p1.photoPoster AND " \
+            "(p1.photoPoster, groupName) IN (SELECT groupOwner, groupName FROM sharedwith WHERE photoID = p1.photoID)))" \
+            "ORDER BY postingdate DESC"
     with connection.cursor() as cursor:
-        cursor.execute(query)
+        cursor.execute(query, (session["username"], session["username"], session["username"]))
     data = cursor.fetchall()
     return render_template("images.html", images=data)
-
 
 @app.route("/image/<image_name>", methods=["GET"])
 def image(image_name):
@@ -65,16 +67,13 @@ def image(image_name):
     if os.path.isfile(image_location):
         return send_file(image_location, mimetype="image/jpg")
 
-
 @app.route("/login", methods=["GET"])
 def login():
     return render_template("login.html")
 
-
 @app.route("/register", methods=["GET"])
 def register():
     return render_template("register.html")
-
 
 @app.route("/loginAuth", methods=["POST"])
 def loginAuth():
@@ -98,7 +97,6 @@ def loginAuth():
     error = "An unknown error has occurred. Please try again."
     return render_template("login.html", error=error)
 
-
 @app.route("/registerAuth", methods=["POST"])
 def registerAuth():
     if request.form:
@@ -111,7 +109,7 @@ def registerAuth():
 
         try:
             with connection.cursor() as cursor:
-                query = "INSERT INTO person (username, password, fname, lname) VALUES (%s, %s, %s, %s)"
+                query = "INSERT INTO person (username, password, firstName, lastName) VALUES (%s, %s, %s, %s)"
                 cursor.execute(query, (username, hashedPassword, firstName, lastName))
         except pymysql.err.IntegrityError:
             error = "%s is already taken." % (username)
@@ -122,32 +120,60 @@ def registerAuth():
     error = "An error has occurred. Please try again."
     return render_template("register.html", error=error)
 
-
 @app.route("/logout", methods=["GET"])
 def logout():
     session.pop("username")
     return redirect("/")
 
-
 @app.route("/uploadImage", methods=["POST"])
 @login_required
 def upload_image():
+    query = "SELECT * FROM belongto WHERE member_username = %s"
+    with connection.cursor() as cursor:
+        cursor.execute(query, (session["username"]))
+    data = cursor.fetchall()
+
     if request.files:
         image_file = request.files.get("imageToUpload", "")
         image_name = image_file.filename
-        filepath = os.path.join(IMAGES_DIR, image_name)
-        image_file.save(filepath)
-        query = "INSERT INTO photo (timestamp, filePath) VALUES (%s, %s)"
-        with connection.cursor() as cursor:
-            cursor.execute(query, (time.strftime('%Y-%m-%d %H:%M:%S'), image_name))
+        filePath = os.path.join(IMAGES_DIR, image_name)
+        image_file.save(filePath)
+
+        requestData = request.form
+        captionInput = requestData["captionInput"]
+
+        if "allFollowersBool" in requestData:
+            allFollowersBool = requestData["allFollowersBool"]
+
+            query = "INSERT INTO photo (postingdate, filepath, allFollowers, caption, photoPoster) VALUES (%s, %s, %s, %s, %s)"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (time.strftime('%Y-%m-%d %H:%M:%S'), image_name, allFollowersBool, captionInput, session["username"]))
+        else:
+            friendGroups = requestData.getlist("fgroup")
+
+            query = "INSERT INTO photo (postingdate, filepath, allFollowers, caption, photoPoster) VALUES (%s, %s, %s, %s, %s)"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (time.strftime('%Y-%m-%d %H:%M:%S'), image_name, "0", captionInput, session["username"]))
+
+            query2 = "SELECT LAST_INSERT_ID() FROM photo"
+            with connection.cursor() as cursor:
+                cursor.execute(query2)
+            lastInsertedId = cursor.fetchone()
+
+            query3 = "INSERT INTO sharedwith (groupOwner, groupName, photoID) VALUES (%s, %s, %s)"
+            for fgroup in friendGroups:
+                friendGroupArr = fgroup.split("-")
+
+                with connection.cursor() as cursor:
+                    cursor.execute(query3, (friendGroupArr[0], friendGroupArr[1], lastInsertedId["LAST_INSERT_ID()"]))
+
         message = "Image has been successfully uploaded."
-        return render_template("upload.html", message=message)
+        return render_template("upload.html", message=message, groups=data)
     else:
         message = "Failed to upload image."
-        return render_template("upload.html", message=message)
-
+        return render_template("upload.html", message=message, groups=data)
 
 if __name__ == "__main__":
     if not os.path.isdir("images"):
         os.mkdir(IMAGES_DIR)
-    app.run()
+    app.run(debug=True)
